eeccad3477fda8cc8ca7422d98d66e5bd234e0ee
A2AD2G4E <57566842+A2AD2G4E@users.noreply.github.com>
2025-01-13 09:21:18 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,46 @@
+resource "aws_security_group" "ec2_security_group" {
+  name_prefix = var.security_group_name
+  description = "Security group for EC2 instance"
+
+  ingress {
+    description      = "Allow SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "Allow HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description      = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "EC2 Security Group"
+  }
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  security_groups = [
+    aws_security_group.ec2_security_group.name
+  ]
+
+  tags = {
+    Name = "MyTerraformInstance"
+  }
+}
